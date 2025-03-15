ba0c28454d013c48a1ee5fb748cf35d3a59fc237
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-03-15 20:17:25 -0300

    HEY-3: should be able to create a new question bigger than 255 characters

---
+++ tests/Feature/CreateAQuestionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+// Utilizando pest, temos um conceito mais simplificado para escrita
+
+// a diferença de test e it é que uma função it() adiciona "it" na frente
+
+use App\Models\User;
+
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, post};
+
+it('should be able to create a new question bigger than 255 characters', function () {
+    // Arrange :: preparar
+    $user = User::factory()->create();
+
+    actingAs($user);
+    // função que faz eu logar no meu teste como usuário
+
+    // Act :: agir
+    $request = post(route('question.store'), [
+        'question' => str_repeat('*', 260) . '?',
+
+    ]);
+    // Assert :: verificar
+    $request->assertRedirect(route('dashboard'));
+    assertDatabaseCount('question', 1);
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
+
+});
+
+it('should check if ends with question mark ?', function () {
+
+});
+
+it('should have at least 10 characters', function () {
+
+});
+
+/*
+test('titulo qualquer', function() {
+    expect(true)->toBeTrue();
+});
+ */
