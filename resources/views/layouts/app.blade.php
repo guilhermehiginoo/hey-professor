be1a77d383810b77c0d4bfc8f0c3dc01f4fe7e7b
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-03-16 01:22:32 -0300

    HEY-3: Add dark mode support with theme toggle and Flowbite integration
    Introduced a theme toggle feature to switch between light and dark modes, using localStorage to remember user preference. Integrated Flowbite for improved UI components and updated Tailwind config to support Flowbite plugins. Adjusted layouts, assets, and scripts to enable seamless dark mode functionality.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -32,5 +31,15 @@
                 {{ $slot }}
             </main>
         </div>
+        <script>
+            // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+            if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark')
+            }
+        </script>
+
+
     </body>
 </html>
