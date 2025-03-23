8260d097e9d93ce5c8a1f54c671c7cf75c2b506d
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-03-23 14:26:32 -0300

    HEY-4: Add functionality to unlike a question
    Introduce an UnlikeController to handle question unlikes. Update the User model with an `unlike` method to manage vote changes. Adjust routes, views, and tests to support and verify the unlike feature.

--- tests/Feature/VoteAQuestionTest.php
+++ tests/Feature/VoteAQuestionTest.php
@@ -35,3 +35,35 @@ it('should not be to like more than 1 time', function () {
 
     expect($user->votes()->where('question_id', '=', $question->id)->get())->toHaveCount(1);
 });
+
+it('should be able to unlike a question', function () {
+    $user     = User::factory()->create();
+    $question = App\Models\Question::factory()->create();
+
+    actingAs($user);
+
+    post(route('question.unlike', $question))
+        ->assertRedirect();
+
+    assertDatabaseHas('votes', [
+        'question_id' => $question->id,
+        'like'        => 0,
+        'unlike'      => 1,
+        'user_id'     => $user->id,
+    ]);
+
+    // SELECT * FROM votes WHERE question_id = ?
+    // AND unlike = 1 AND ununlike = 0 AND USER_ID = ? EXISTS
+
+});
+
+it('should not be to unlike more than 1 time', function () {
+    $user     = User::factory()->create();
+    $question = App\Models\Question::factory()->create();
+
+    actingAs($user);
+
+    post(route('question.unlike', $question));
+
+    expect($user->votes()->where('question_id', '=', $question->id)->get())->toHaveCount(1);
+});
