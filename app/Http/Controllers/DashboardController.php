fe2655d2556dbe4e611dc4a393d33fec8412e312
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-03-23 20:31:36 -0300

    HEY-4: Update question vote handling and improve project configs

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,10 +9,13 @@ class DashboardController extends Controller
 {
     public function __invoke(): View
     {
-        // quando digo que a function vai retornar uma view, posso passar parâmetros
-        // questions é a variável dentro da view, que será acessado dentro de 'dashboard'
         return view('dashboard', [
-            'questions' => Question::all(),
+            'questions' => Question::withSum('votes', 'like')
+                ->withSum('votes', 'unlike')
+                ->get(),
         ]);
     }
 }
+
+// quando digo que a function vai retornar uma view, posso passar parâmetros
+// questions é a variável dentro da view, que será acessado dentro de 'dashboard'
