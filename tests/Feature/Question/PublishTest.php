2f1556a0f2b3ceba1cc6adcd173ea3033c3976ec
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-03-23 21:36:09 -0300

    hey-5: Add question publishing feature
    Introduce a new endpoint and controller for publishing questions. A `PUT` route is added to allow questions to transition from draft to published. Includes corresponding tests to ensure functionality works as intended.

---
+++ tests/Feature/Question/PublishTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use function Pest\Laravel\{actingAs, put};
+
+it('should be abe to publish a question', function () {
+    $user     = \App\Models\User::factory()->create();
+    $question = \App\Models\Question::factory()->create(['draft' => true]);
+
+    actingAs($user);
+
+    put(route('question.publish', $question))
+        ->assertRedirect();
+
+    $question->refresh();
+    expect($question)
+        ->draft->toBeFalse();
+
+});
