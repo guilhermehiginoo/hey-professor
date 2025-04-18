d0796979805f729c348b482e45c16b5c29b5375d
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-04-18 15:37:58 -0300

    hey-6: Validate question format and add related tests
    Implemented validation to ensure questions have at least 10 characters and end with a question mark. Updated the store and update methods in QuestionController to enforce these rules. Added comprehensive tests to verify the validation logic and database state.

--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -2,7 +2,7 @@
 
 use App\Models\{Question, User};
 
-use function Pest\Laravel\{actingAs, put};
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, put};
 
 it('should be able to update the question in database', function () {
     $user     = User::factory()->create();
@@ -41,5 +41,65 @@ it('should make sure that only the person who has created the question can updat
 
     actingAs($rightUser);
     put(route('question.update', $question), ['question' => 'New Question'])->assertRedirect();
+
+});
+
+it('should be able to update a new question bigger than 255 characters', function () {
+    // Arrange :: preparar
+    $user     = User::factory()->create();
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+    actingAs($user);
+    // função que faz eu logar no meu teste como usuário
+
+    // Act :: agir
+    $request = put(route('question.update', $question), [
+        'question' => str_repeat('*', 260) . '?',
+
+    ]);
+    // Assert :: verificar
+    $request->assertRedirect();
+    assertDatabaseCount('questions', 1);
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
+
+});
+
+it('should check if ends with question mark ?', function () {
+    // Arrange :: preparar
+    $user     = User::factory()->create();
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+    actingAs($user);
+
+    // Act :: agir
+    $request = put(route('question.update', $question), [
+        'question' => str_repeat('*', 10),
+
+    ]);
+
+    // Assert :: verificar
+    $request->assertSessionHasErrors([
+        'question' => 'Are you sure that is a question? It is missing the question mark (?) in the end.',
+    ]);
+
+    assertDatabaseHas('questions', [
+        'question' => $question->question,
+    ]);
+});
+
+it('should have at least 10 characters', function () {
+    $user     = User::factory()->create();
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+
+    actingAs($user);
+
+    $request = put(route('question.update', $question), [
+        'question' => str_repeat('*', 8) . '?',
+
+    ]);
+
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+
+    assertDatabaseHas('questions', [
+        'question' => $question->question,
+    ]);
 
 });
