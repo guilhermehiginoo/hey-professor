2298b102e29fca5283ddf0e8b03d95a3975d4a5a
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-04-25 16:20:15 -0300

    hey-24: Update question listing with ordering and search feature

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -22,7 +22,7 @@ it('should list all the questions', function () {
     }
 });
 
-it('should paginated the questions', function () {
+it('should paginate the questions', function () {
     $user = User::factory()->create();
     Question::factory()->count(20)->create();
 
@@ -30,4 +30,28 @@ it('should paginated the questions', function () {
 
     get(route('dashboard'))
         ->assertViewHas('questions', fn ($value) => $value instanceof LengthAwarePaginator);
+});
+
+it('should order by like and unlike, most liked question should be at the top, most unliked questions should be in the bottom', function () {
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+    Question::factory()->count(5)->create();
+
+    $mostLikedQuestion = Question::find(3);
+    $user->like($mostLikedQuestion);
+
+    $mostUnlikedQuestion = Question::find(1);
+    $secondUser->unlike($mostUnlikedQuestion);
+
+    actingAs($user);
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) {
+
+            expect($questions)
+                ->first()->id->toBe(3)
+                ->and($questions)
+                ->last()->id->toBe(1);
+
+            return true;
+        });
 });
