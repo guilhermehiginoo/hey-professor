e42b492d799deb98c007402cbfc6fb8d8525a3dc
Guilherme Higino <guilhermehiginoo@gmail.com>
2025-05-15 18:41:49 -0300

    hey-10: adding icon when search is empty

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,11 +8,23 @@
     <x-container>
         {{-- listagem --}}
         <div class="dark:text-gray-400 space-y-4">
-            @foreach($questions as $item)
-                <x-question :question="$item"/>
-            @endforeach
+            <form action="{{route('dashboard')}}" method="get" class="flex items-center space-x-2">
+                @csrf
+                <x-text-input type="text" name="search" value="{{ request()->search }}" class="w-full" placeholder="Search for a question..."/>
+                <x-btn.primary type="submit">Search</x-btn.primary>
+            </form>
 
-            {{ $questions->withQueryString()->links() }}
+            @if($questions->isEmpty())
+                <div class="dark-text-gray-300 text-center flex justify-center">
+                    <x-draw.searching class="w-80 h-80"/>
+
+                </div>
+            @else
+                @foreach($questions as $item)
+                    <x-question :question="$item"/>
+                @endforeach
+                    {{ $questions->withQueryString()->links() }}
+            @endif
 
         </div>
     </x-container>
